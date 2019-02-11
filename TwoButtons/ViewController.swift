8b0b07881cb2ec16caf675a620e4c9bf6cd53ad9
Sim Yong Seng <simyseng@gmail.com>
2019-02-11 19:55:16 +0700

    Initial Commit

---
+++ TwoButtons/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwoButtons
+//
+//  Created by Sim Yong Seng on 11/2/19.
+//  Copyright © 2019 Sim Yong Seng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
